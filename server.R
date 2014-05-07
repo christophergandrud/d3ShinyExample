ab8a230b57694365152aaf7fc33b91a5ccde50a4
Christopher Gandrud <christopher.gandrud@gmail.com>
2014-05-07 10:08:24 +0100

    first commit

---
+++ server.R
@@ -0,0 +1,28 @@
+#### Do once on Page Load ####
+# Load packages
+library(RCurl)
+library(d3Network)
+
+# Load data once
+URL <- "https://raw.githubusercontent.com/christophergandrud/d3Network/master/JSONdata/miserables.json"
+MisJson <- getURL(URL, ssl.verifypeer = FALSE)
+
+# Convert JSON arrays into data frames
+MisLinks <- JSONtoDF(jsonStr = MisJson, array = "links")
+MisNodes <- JSONtoDF(jsonStr = MisJson, array = "nodes")
+
+# Create individual ID
+MisNodes$ID <- 1:nrow(MisNodes)
+
+#### Shiny ####
+shinyServer(function(input, output) {
+
+    output$networkPlot <- renderPrint({
+        d3ForceNetwork(Nodes = MisNodes, 
+            Links = MisLinks,  
+            Source = "source", Target = "target", 
+            Value = "value", NodeID = "name", 
+            Group = "group", width = 550, height = 400, 
+            opacity = input$slider, standAlone = FALSE)
+    })
+})
---
+++ ui.R
@@ -0,0 +1,32 @@
+library(shiny)
+
+shinyUI(fluidPage(
+
+    # Load d3.js
+    tags$head(
+        tags$script(src = 'http://d3js.org/d3.v3.min.js')
+    ),
+
+    # Application title
+    titlePanel('d3Network Shiny Example'),
+    p('This is an example of using',
+    a(href = 'http://christophergandrud.github.io/d3Network/', 'd3Network'),
+        'with',
+        a(href = 'http://shiny.rstudio.com/', 'Shiny', 'web apps.')
+    ),
+
+    # Sidebar with a slider input for node opacity
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput('slider', label = 'Choose node opacity',
+                min = 0, max = 1, step = 0.01, value = 0.5
+            )
+    ),
+
+    # Show network graph
+    mainPanel(
+        h2('This is where the network plot should be.'),
+        htmlOutput('networkPlot')
+    )
+  )
+))
